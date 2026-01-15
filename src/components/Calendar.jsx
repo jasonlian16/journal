c72172aa5e4a837baa209ccb514df432ce56f351
Jason Lian <jlian16@bu.edu>
2026-01-15 18:22:27 -0500

    Imported Calendar from React

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+export const CalendarComponent = () =>{
+    const [date, setDate] = useState(new Date());
+    return(
+        <div>
+            <Calendar onChange={setDate} value={date}/>
+        </div>
+    )
+}
